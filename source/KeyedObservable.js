49025736767cc4f7be488aa4451acb223a72607a
chkt <ck@christoph-kettelhoit.de>
2015-09-26 21:39:41 +0200

    Changed KeyedObservable to use Notifier

--- source/KeyedObservable.js
+++ source/KeyedObservable.js
@@ -1,22 +1,26 @@
+import Notifier, * as _notify from './Notifier';
+
+
+const _ERROP = "invalid property manipulation";
+
 const _PROP_CHILD = Symbol();
 
-const _KEY_OBSERVE = Symbol();
-const _KEY_OBSERVE_ANY = Symbol();
-const _KEY_RELEASE = Symbol();
-const _KEY_RELEASE_ANY = Symbol();
-const _KEY_RELEASE_ALL = Symbol();
 
-const _KEY_ADD = Symbol();
-const _KEY_REMOVE = Symbol();
+export const ADD_PROPS = Symbol('addProperties');
+export const REMOVE_PROPS = Symbol('removeProperties');
 
+export const OBSERVE = Symbol('observe');
+export const OBSERVE_ANY = Symbol('observeAny');
+export const RELEASE = Symbol('release');
+export const RELEASE_ANY = Symbol('releaseAny');
+export const RELEASE_ALL = Symbol('releaseAll');
 
 
 const _factoryKeyed = new WeakMap();
 const _factoryIndexed = new WeakMap();
 
-const _attrValue = new WeakMap();
-const _attrObserve = new WeakMap();
-const _anyObserve = new WeakMap();
+const _value = new WeakMap();
+const _notifier = new WeakMap();
 
 
 
@@ -28,7 +32,7 @@ const _anyObserve = new WeakMap();
  * @private
  */
 function _createGetter(ins, name) {
-	return () => _attrValue.get(ins)[name];
+	return () => _value.get(ins)[name];
 }
 
 /**
@@ -39,15 +43,17 @@ function _createGetter(ins, name) {
  * @private
  */
 function _createSetter(ins, name) {
+	const notify = _notifier.get(ins);
+
 	return now => {
-		const vals = _attrValue.get(ins);
+		const vals = _value.get(ins);
 		const was = vals[name];
 
 		if (was === now) return;
 
 		vals[name] = now;
 
-		_notify.call(ins, name, now, was);
+		notify.queue(name, _notify.TYPE_UPDATE, now, was);
 	};
 }
 
@@ -59,7 +65,7 @@ function _createSetter(ins, name) {
  */
 function _addKeys(source) {
 	const spec = {};
-	const vals = _attrValue.get(this);
+	const vals = _value.get(this), notify = _notifier.get(this);
 
 	const fKeyed = _factoryKeyed.get(this);
 	const fIndexed = _factoryIndexed.get(this);
@@ -67,13 +73,17 @@ function _addKeys(source) {
 	for (let prop in source) {
 		if (!source.hasOwnProperty(prop)) continue;
 
-		if (prop in vals) throw new Error();
+		if (prop in vals) throw new Error(_ERROP);
 
 		const val = source[prop];
 
 		if (Array.isArray(val)) {
+			const ins = fIndexed.call(this, prop, source[prop]);
+
+			_notifier.get(ins).setCascadeParent(notify, prop);
+
 			spec[prop] = {
-				value : fIndexed.call(this.constructor, prop, source[prop]),
+				value : ins,
 				configurable : true,
 				enumerable : true
 			};
@@ -81,8 +91,12 @@ function _addKeys(source) {
 			vals[prop] = _PROP_CHILD;
 		}
 		else if (val instanceof Object) {
+			const ins = fKeyed.call(this, prop, source[prop]);
+
+			_notifier.get(ins).setCascadeParent(notify, prop);
+
 			spec[prop] = {
-				value : fKeyed.call(this.constructor, prop, source[prop]),
+				value : ins,
 				configurable : true,
 				enumerable : true
 			};
@@ -99,7 +113,7 @@ function _addKeys(source) {
 
 			vals[prop] = source[prop];
 
-			_notify.call(this, prop, source[prop], undefined);
+			notify.queue(prop, _notify.TYPE_ADD, source[prop], undefined);
 		}
 	}
 
@@ -108,60 +122,13 @@ function _addKeys(source) {
 
 /**
  * Removes the keys represented by source
- * @param {String[]} source
+ * @param {Object} source
  * @private
  */
 function _removeKeys(source) {
 	//IMPLEMENT
 }
 
-
-/**
- * Calls all registered observers
- * @param {Set} cbs
- * @param {String} name - The property name
- * @param {*} now
- * @param {*} was
- * @private
- */
-function _call(cbs, name, now, was) {
-	if (cbs.size === 0) return;
-
-	for (let cb of cbs) {
-		try {
-			cb.call(this, name, now, was);
-		}
-		catch (err) {
-			console.error(err);		//IMPLEMENT
-		}
-	}
-}
-
-/**
- * Notifies all property and generic observers of changes
- * @param {String} name
- * @param {*} now
- * @param {*} was
- * @private
- */
-function _notify(name, now, was) {
-	const obs = _attrObserve.get(this);
-
-	if ((name in obs)) _call.call(this, obs[name], name, now, was);
-
-	_call.call(this, _anyObserve.get(this), name, now, was);
-}
-
-
-
-export const ADD_PROPS = _KEY_ADD;
-export const REMOVE_PROPS = _KEY_REMOVE;
-
-export const OBSERVE = _KEY_OBSERVE;
-export const OBSERVE_ANY = _KEY_OBSERVE_ANY;
-export const RELEASE = _KEY_RELEASE;
-export const RELEASE_ANY = _KEY_RELEASE_ANY;
-export const RELEASE_ALL = _KEY_RELEASE_ALL;
 
 
 export default class KeyedObservable {
@@ -189,15 +156,14 @@ export default class KeyedObservable {
 		_factoryKeyed.set(this, (name, keys) => new KeyedObservable(keys));
 		_factoryIndexed.set(this, (name, indexes) => new IndexedObservable(indexed));
 
-		_attrValue.set(this, {});
-		_attrObserve.set(this, {});
-		_anyObserve.set(this, new Set());
+		_value.set(this, {});
+		_notifier.set(this, new Notifier(this));
 
 		_addKeys.call(this, source);
 	}
 
 
-	[_KEY_ADD](source) {
+	[ADD_PROPS](source) {
 		if (typeof source !== 'object' || source === null) throw new TypeError();
 
 		_addKeys.call(this, source);
@@ -205,68 +171,48 @@ export default class KeyedObservable {
 		return this;
 	}
 
+	[REMOVE_PROPS](source) {
+		if (!Array.isArray(source)) throw new TypeError();
 
-	[_KEY_OBSERVE](name, cb) {
-		if (
-			typeof name !== 'string' || name === '' ||
-			typeof cb !== 'function'
-		) throw new TypeError();
-
-		const obs = _attrObserve.get(this);
-
-		if (!(name in obs)) obs[name] = new Set();
-
-		obs[name].add(cb);
+		_removeKeys.call(this, source);
 
 		return this;
 	}
 
-	[_KEY_OBSERVE_ANY](cb) {
-		if (typeof cb !== 'function') throw new TypeError();
 
-		_anyObserve.get(this).add(cb);
-
-		return this;
-	}
-
-	[_KEY_RELEASE](name, cb) {
-		if (
-			typeof name !== 'string' || name === '' ||
-			typeof cb !== 'function'
-		) throw new TypeError();
-
-		const obs = _attrObserve.get(this);
-
-		if (!(name in obs)) return this;
-
-		obs[name].delete(cb);
-
-		if (obs[name].size === 0) delete obs[name];
+	[OBSERVE](name, cb) {
+		_notifier.get(this).addNamedListener(name, cb);
 
 		return this;
 	}
 
-	[_KEY_RELEASE_ANY](cb) {
-		if (typeof cb !== 'function') throw new TypeError();
-
-		_anyObserve.get(this).delete(cb);
+	[OBSERVE_ANY](cb) {
+		_notifier.get(this).addListener(cb);
 
 		return this;
 	}
 
-	[_KEY_RELEASE_ALL](name) {
-		if (typeof name !== 'string' || name === '') throw new TypeError();
+	[RELEASE](name, cb) {
+		_notifier.get(this).removeNamedListener(name, cb);
 
-		const obs = _attrObserve.get(this);
+		return this;
+	}
 
-		if (name in obs) delete obs[name];
+	[RELEASE_ANY](cb) {
+		_notifier.get(this).removeListener(cb);
+
+		return this;
+	}
+
+	[RELEASE_ALL](name) {
+		_notifier.get(this).removeAllNamedListeners(name);
 
 		return this;
 	}
 
 
 	toJSON() {
-		const vals = _attrValue.get(this), res = {};
+		const vals = _value.get(this), res = {};
 
 		for (let prop in vals) res[prop] = vals[prop] === _PROP_CHILD ? this[prop].toJSON() : vals[prop];
 
