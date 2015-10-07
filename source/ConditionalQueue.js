fb5190972a58fa3ec383e09b75a1b64044ec4c04
chkt <ck@christoph-kettelhoit.de>
2015-10-07 22:19:35 +0200

    Added typechecks

--- source/ConditionalQueue.js
+++ source/ConditionalQueue.js
@@ -4,6 +4,11 @@ import Queue from 'qee';
 
 export default class FactoryQueue extends Queue {
 	process(source, prop, val) {
+		if (
+			typeof source !== 'object' || source === null ||
+			typeof prop !== 'string' || prop === ''
+		) throw new TypeError();
+
 		for (let fn of this) {
 			const res = fn.call(source, prop, val);
 
