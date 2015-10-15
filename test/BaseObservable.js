9472da573700dc7345559d485c8d557d00b52673
chkt <ck@christoph-kettelhoit.de>
2015-10-16 01:32:49 +0200

    Added #[SET_PROPERTIES] tests

--- test/BaseObservable.js
+++ test/BaseObservable.js
@@ -1,7 +1,7 @@
 import _assert from 'assert';
 
 import Observable, * as _observe from '../source/BaseObservable';
-import Notifier from '../source/Notifier';
+import Notifier, * as _notify from '../source/Notifier';
 
 
 
@@ -13,6 +13,20 @@ const _genObj = function* (source) {
 
 		yield [prop, source[prop]];
 	}
+};
+
+const _factType = Symbol();
+
+const _factObs = function(prop, val) {
+	let target;
+
+	if (typeof val === 'object' && val !== null) {
+		target = new Observable(_genObj, _factType);
+
+		target[_observe.SET_PROPERTIES](val);
+	}
+
+	return target;
 };
 
 
@@ -152,6 +166,269 @@ describe('BaseObservable', () => {
 			_assert(ins instanceof Observable);
 		});
 	});
+
+
+	describe('#[SET_PROPERTIES]', () => {
+		it("should accept an object argument", () => {
+			const ins = new Observable(_genEmpty);
+
+			_assert.doesNotThrow(() => ins[_observe.SET_PROPERTIES]({}));
+		});
+
+		it("should only accept an object argument", () => {
+			const ins = new Observable(_genEmpty());
+			const set = _observe.SET_PROPERTIES;
+
+			_assert.throws(() => ins[set](true), TypeError);
+			_assert.throws(() => ins[set](1), TypeError);
+			_assert.throws(() => ins[set]("1"), TypeError);
+			_assert.throws(() => ins[set](null), TypeError);
+			_assert.throws(() => ins[set](() => 1), TypeError);
+			_assert.throws(() => ins[set](Symbol()), TypeError);
+		});
+
+		it("should add scalar properties to the instance", () => {
+			const ins = new Observable(_genObj);
+
+			ins[_observe.SET_PROPERTIES]({
+				a : 1,
+				b : 2,
+				c : 3
+			});
+
+			_assert.strictEqual(ins.a, 1);
+			_assert.strictEqual(ins.b, 2);
+			_assert.strictEqual(ins.c, 3);
+		});
+
+		it("should update scalar properties on the instance", () => {
+			const ins = new Observable(_genObj);
+			const set = _observe.SET_PROPERTIES;
+
+			ins[set]({
+				a : 1
+			});
+
+			ins[set]({
+				a : 2
+			});
+
+			_assert.strictEqual(ins.a, 2);
+		});
+
+		it("should remove scalar properties from the instance", () => {
+			const ins = new Observable(_genObj);
+			const set = _observe.SET_PROPERTIES;
+
+			ins[set]({
+				a : 1,
+				b : 2,
+				c : 3
+			});
+
+			ins[set]({});
+
+			_assert(!('a' in ins));
+			_assert(!('b' in ins));
+			_assert(!('c' in ins));
+		});
+
+		it("should add nested properties to the instance", () => {
+			Observable.configure(_factType, _factObs);
+
+			const ins = new Observable(_genObj, _factType);
+
+			ins[_observe.SET_PROPERTIES]({
+				a : {
+					a : 1,
+					b : 2,
+					c : 3
+				}
+			});
+
+			_assert(ins.a instanceof Observable);
+			_assert.strictEqual(ins.a.a, 1);
+			_assert.strictEqual(ins.a.b, 2);
+			_assert.strictEqual(ins.a.c, 3);
+		});
+
+		it("should update nested properties on the instance", () => {
+			Observable.configure(_factType, _factObs);
+
+			const ins = new Observable(_genObj, _factType);
+			const set = _observe.SET_PROPERTIES;
+
+			ins[set]({
+				a : {
+					a : 1
+				}
+			});
+
+			const was = ins.a;
+
+			ins[set]({
+				a : {
+					a : 2
+				}
+			});
+
+			_assert(ins.a instanceof Observable);
+			_assert.strictEqual(ins.a, was);
+			_assert.strictEqual(ins.a.a, 2);
+		});
+
+		it("should remove nested properties from the instance", () => {
+			Observable.configure(_factType, _factObs);
+
+			const ins = new Observable(_genObj, _factType);
+			const set = _observe.SET_PROPERTIES;
+
+			ins[set]({
+				a : {
+					a : 1
+				}
+			});
+
+			ins[set]({ a : {}});
+
+			_assert(ins.a instanceof Observable);
+			_assert(!('a' in ins.a));
+		});
+
+		it("should notify when creating scalar properties", done => {
+			const ins = new Observable(_genObj);
+
+			ins[_observe.SET_PROPERTIES]({ a : 1 });
+
+			_observe.getNotifier.call(ins).addListener((now, was, meta) => {
+				if (
+					now === 1 &&
+					was === undefined &&
+					meta.type === _notify.TYPE_ADD
+				) done();
+				else done(new Error());
+			});
+		});
+
+		it("should notify when updating scalar properties", done => {
+			const ins = new Observable(_genObj);
+			const set = _observe.SET_PROPERTIES;
+
+			ins[set]({ a : 1 });
+			ins[set]({ a : 2 });
+
+			_observe.getNotifier.call(ins).addListener((now, was, meta) => {
+				if (meta.type === _notify.TYPE_ADD) return;
+				else if (
+					now === 2 &&
+					was === 1 &&
+					meta.type === _notify.TYPE_UPDATE
+				) done();
+				else done(new Error());
+			});
+		});
+
+		it("should notify when removing scalar properties", done => {
+			const ins = new Observable(_genObj);
+			const set = _observe.SET_PROPERTIES;
+
+			ins[set]({ a : 1});
+			ins[set]({});
+
+			_observe.getNotifier.call(ins).addListener((now, was, meta) => {
+				if (meta.type === _notify.TYPE_ADD) return;
+				else if (
+					now === undefined &&
+					was === 1 &&
+					meta.type === _notify.TYPE_REMOVE
+				) done();
+				else done(new Error());
+			});
+		});
+
+		it("should notify when creating nested properties", done => {
+			Observable.configure(_factType, _factObs);
+
+			const ins = new Observable(_genObj, _factType);
+
+			ins[_observe.SET_PROPERTIES]({
+				a : {
+					a : 1
+				}
+			});
+
+			_observe.getNotifier.call(ins).addListener((now, was, meta) => {
+				if (meta.path.length === 1) return;
+				else if (
+					now === 1 &&
+					was === undefined &&
+					meta.type === _notify.TYPE_ADD
+				) done();
+				else done(new Error());
+			});
+		});
+
+		it("should notify when updating nested properties", done => {
+			Observable.configure(_factType, _factObs);
+
+			const ins = new Observable(_genObj, _factType);
+			const set = _observe.SET_PROPERTIES;
+
+			ins[set]({
+				a : {
+					a : 1
+				}
+			});
+
+			ins[set]({
+				a : {
+					a : 2
+				}
+			});
+
+			_observe.getNotifier.call(ins).addListener((now, was, meta) => {
+				if (meta.type === _notify.TYPE_ADD) return;
+				else if (
+					now === 2 &&
+					was === 1 &&
+					meta.type === _notify.TYPE_UPDATE
+				) done();
+				else done(new Error());
+			});
+		});
+
+		it("should notify when removing nested properties", done => {
+			Observable.configure(_factType, _factObs);
+
+			const ins = new Observable(_genObj, _factType);
+			const set = _observe.SET_PROPERTIES;
+
+			ins[set]({
+				a : {
+					a : 1
+				}
+			});
+
+			ins[set]({ a : {}});
+
+			_observe.getNotifier.call(ins).addListener((now, was, meta) => {
+				if (meta.type === _notify.TYPE_ADD) return;
+				else if (
+					now === undefined &&
+					was === 1 &&
+					meta.type === _notify.TYPE_REMOVE
+				) done();
+				else done(new Error());
+			});
+		});
+
+		it("should be chainable", () => {
+			const ins = new Observable(_genObj);
+
+			_assert.strictEqual(ins[_observe.SET_PROPERTIES]({}), ins);
+		});
+	});
+
 
 	describe('#[Symbol.iterator]', () => {
 		it("should return an iterator", () => {
