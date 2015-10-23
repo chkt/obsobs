97bf5a46ee13ea2199d41b2aaf2f570d37f78120
chkt <ck@christoph-kettelhoit.de>
2015-10-24 00:55:39 +0200

    Added target argument to process Changed return value on failure to null

--- source/ConditionalQueue.js
+++ source/ConditionalQueue.js
@@ -3,18 +3,18 @@ import Queue from 'qee';
 
 
 export default class FactoryQueue extends Queue {
-	process(source, prop, val) {
+	process(source, prop, val, target) {
 		if (
 			typeof source !== 'object' || source === null ||
 			typeof prop !== 'string' || prop === ''
 		) throw new TypeError();
 
 		for (let fn of this) {
-			const res = fn.call(source, prop, val);
+			const res = fn.call(source, prop, val, target);
 
 			if (res !== undefined) return res;
 		}
 
-		return val;
+		return null;
 	}
 }
